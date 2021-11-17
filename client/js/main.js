970fb3493e7239fb7ff1d989a9914091695619b8
かねけん <b1r2d3q4@gmail.com>
2021-11-17 14:17:40 +0900

    fix: 追加要素に対するクリックイベント

--- client/js/main.js
+++ client/js/main.js
@@ -1,49 +1,62 @@
 const url = 'http://localhost:8080/api/page';
 
 function format(id, title, message) {
-    let card = '';
-    card += '<li id="post-' + id + '" class="card">';
-    card += '<div class="left">' + message + '</div>';
-    card += '<div class="right"><div class="button-block"><button class="edit">編集</button><button class="delete">削除</button></div><p class="name">' + title + '</p></div>'
-    card += '</li>';
-    return card;
+  let card = '';
+  card += '<li id="post-' + id + '" class="card" data-id="' + id + '">';
+  card += '<div class="left">' + message + '</div>';
+  card += '<div class="right"><div class="button-block"><button class="edit">編集</button><button class="delete">削除</button></div><p class="name">' + title + '</p></div>'
+  card += '</li>';
+  return card;
 }
-
-
 
 // 一覧取得
 $.ajax({
-    type: 'GET',
-    url: url
+  type: 'GET',
+  url: url
 }).done(function(data){
-    data = JSON.parse(data)
-    data.forEach(function(post) {
-        $('#lists').append(format(post[0], post[1], post[2]));
-    });
+  data = JSON.parse(data)
+  data.forEach(function(post) {
+    $('#lists').append(format(post[0], post[1], post[2]));
+  });
 });
 
 $('#post').on('click', () => {
-    let title = $('#title').val();
-    let message = $('#message').val();
+  let title = $('#title').val();
+  let message = $('#message').val();
 
-    $.ajax({
-        type: 'POST',
-        url: url,
-        data: {
-            "title": title,
-            "message": message
-        }
-    }).done(function(data){
-        window.location.reload();
-    });
+  $.ajax({
+    type: 'POST',
+    url: url,
+    data: {
+        "title": title,
+        "message": message
+    }
+  }).done(function(data){
+    window.location.reload();
+  });
 });
 
-$('.card .edit').on('click', () => {
-    // TODO: 編集が押された時の処理
-    alert('編集が押されました');
+
+$('body').on('click', '.edit', () => {
+  alert('編集が押されました');
+  // 投稿idを取得
+
+  // 投稿idからデータを取得して、タイトルとメッセージを格納
+
+  // ボタンテキストを「編集」に変更
+
+  // 更新APIを叩く
+
 });
 
-$('.card .delete').on('click', () => {
-    // TODO: 削除が押された時の処理
-    alert('削除が押されました');
+$('body').on('click', '.delete', () => {
+  // 投稿idを取得
+  alert('削除が押されました');
+  console.log($(this).parents('.card'));
+
+  // アラートを出す
+
+  // 投稿idをもとに削除APIを叩く
+
 });
+
